f8c42bba10dc7eab969ba931a59f95e1a90d18f4
Macking Liu <macking@live.com>
2014-09-10 21:00:23 +0800

    add part 3

--- corr.R
+++ corr.R
@@ -1,13 +1,25 @@
 corr <- function(directory, threshold = 0) {
         ## 'directory' is a character vector of length 1 indicating
         ## the location of the CSV files
-
-		fileseq <- formatC(id, width = 3, flag = 0)
-		dir <- paste(directory, fileseq, sep="/")
-		dir1 <- paste(dir,".csv",sep="")
-		dataset <- lapply(dir1, function(i) {read.csv(i)})
-		tt <- do.call(rbind, dataset)
-	
+        files <- list.files(directory)
+		    #fileseq <- formatC(id, width = 3, flag = 0)
+		    dir <- paste(directory, files, sep="/")
+		    #dir1 <- paste(dir,".csv",sep="")
+		    dataset <- lapply(dir, function(i) {
+            tab <- read.csv(i)
+            valid <- na.omit(tab)
+            if(nrow(valid) > threshold) {
+                cor(valid[,2],valid[,3])
+            } else{
+                vector('numeric')
+            }
+        })
+	  
+        tt <- round(do.call(cbind, dataset),digits=5)
+		    #tt <- do.call(rbind,dataset)
+        #print(tt)
+        as.numeric(tt)
+        
         ## 'threshold' is a numeric vector of length 1 indicating the
         ## number of completely observed observations (on all
         ## variables) required to compute the correlation between
