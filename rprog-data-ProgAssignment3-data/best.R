51c2767cf4710e3dd5d98989ec1427a377cf8227
Macking Liu <macking.liu@autodesk.com>
2014-09-16 13:40:45 +0800

    optimise the summit codes

--- rprog-data-ProgAssignment3-data/best.R
+++ rprog-data-ProgAssignment3-data/best.R
@@ -9,40 +9,35 @@ best <- function (stat, outcome) {
     if(!(outcome %in% c("heart attack","heart failure","pneumonia")))
         stop("invalid outcome")
     ## Return hospital name in that state with lowest 30-day death
+    data <- subset(data, State == stat)
     
     if(outcome == "heart attack") {
         keeps <- c("Hospital.Name","State","Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack")
         attack <- data[keeps]
-        attackState <- subset(attack,State == stat & Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack!= "Not Available")
+        attackState <- subset(attack,Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack!= "Not Available")
         attackState[,3] <- as.numeric(attackState[,3])
         attackState <- attackState[order(attackState[,3]),]
         min <- attackState[1,3]
         reduceTab <- subset(attackState, Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack == min)
-        rightLine <- reduceTab[order(reduceTab[,2]),]
-        hospital <- rightLine[1,1]
-        hospital
     } else if(outcome == "heart failure") {
         keeps <- c("Hospital.Name","State","Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure")
         attack <- data[keeps]
-        attackState <- subset(attack,State == stat & Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure!= "Not Available")
+        attackState <- subset(attack,Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure!= "Not Available")
         attackState[,3] <- as.numeric(attackState[,3])
         attackState <- attackState[order(attackState[,3]),]
         min <- attackState[1,3]
         reduceTab <- subset(attackState, Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure == min)
-        rightLine <- reduceTab[order(reduceTab[,2]),]
-        hospital <- rightLine[1,1]
-        hospital
     } else if(outcome == "pneumonia") {
         keeps <- c("Hospital.Name","State","Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia")
         attack <- data[keeps]
-        attackState <- subset(attack,State == stat & Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia!= "Not Available")
+        attackState <- subset(attack,Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia!= "Not Available")
         attackState[,3] <- as.numeric(attackState[,3])
         attackState <- attackState[order(attackState[,3]),]
         min <- attackState[1,3]
         reduceTab <- subset(attackState, Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia <= min)
-        rightLine <- reduceTab[order(reduceTab[,2]),]
-        hospital <- rightLine[1,1]
-        hospital
     }
+    rightLine <- reduceTab[order(reduceTab[,2]),]
+    hospital <- rightLine[1,1]
+    hospital
     ## Rate
 }
